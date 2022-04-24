9fa81dcba503cfcc99d1c81558b6671873001af4
leo <leonidas84s.f@gmail.com>
2022-04-23 19:07:16 -0500

    Modificando descripciones de la prueba

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,8 +1,8 @@
 const MissionCommander = require('./../app/missionCommander');
 
 
-describe("Esto es una suite de pruebas", () => {
-    test('Caso de prueba 1', () => {
+describe("Unit Tests for Mission Commander Class", () => {
+    test('1) Create a mission commander objet', () => {
       const result = 1 + 2 
       expect(result).toBe(3);
     });
