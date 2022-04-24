260ff773ecc0f6fd7ee5cf2db51c5958300657e9
leo <leonidas84s.f@gmail.com>
2022-04-23 19:05:55 -0500

    Importando clase missioncommander

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,3 +1,6 @@
+const MissionCommander = require('./../app/missionCommander');
+
+
 describe("Esto es una suite de pruebas", () => {
     test('Caso de prueba 1', () => {
       const result = 1 + 2 
