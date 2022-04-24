5435c00f83a8b887c5ab727c8673dbb085f288b7
leo <leonidas84s.f@gmail.com>
2022-04-23 19:09:29 -0500

    Agreando a expect la propiedad que se quiera probar

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -5,7 +5,7 @@ describe("Unit Tests for Mission Commander Class", () => {
     test('1) Create a mission commander objet', () => {
         const myMissionCommander = new MissionCommander("Woopa")
 
-      expect(result).toBe(3);
+        expect(myMissionCommander.name).toBe("Woopa");
     });
   })
   
